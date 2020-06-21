19bad4a70a8b875f2315c3ed11e8d294a6c93b12
nikolaev-anton <anton.i.nikolaev@mail.ru>
2020-06-21 22:00:07 +0300

    get_channels - список мест, откуда пришли(узнали о нас) - источников трафика

---
+++ get_channels.php
@@ -0,0 +1,36 @@
+<?php
+include 'include.php';
+header('Content-Type: application/json; charset=UTF-8');
+
+$response = "";
+
+if (!$db_conection = db_connect()) 
+  {
+  finish("db_connect failed" . mysqli_sqlstate($db_conection) . mysqli_error($db_conection));
+  }
+
+
+$sql = "select id,name from CHANNELS";
+
+//получение списка статусов из STATUS
+$result = mysqli_query($db_conection, $sql);
+if (!$result) 
+  {
+  finish("get_channels SELECT error: ". $key ." ". mysqli_sqlstate($db_conection) . mysqli_error($db_conection));
+  };
+
+$channels = array();
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+	{
+	array_push($channels, $row);
+	}
+
+mysqli_free_result($result);
+mysqli_close($db_conection);
+//возвращаю результат
+$response_array = array("error" => false, "channels" => $channels);
+echo json_encode($response_array, JSON_UNESCAPED_UNICODE);
+
+?>
+
+
